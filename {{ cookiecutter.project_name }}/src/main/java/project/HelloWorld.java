bc2cc239a00cdbf94d7033899efeecad4fe65ad9
Larry Tenny <ltenny@gmail.com>
2021-01-28 13:41:24 -0600

    Updated to include environment vars

--- {{ cookiecutter.project_name }}/src/main/java/project/HelloWorld.java
+++ {{ cookiecutter.project_name }}/src/main/java/project/HelloWorld.java
@@ -1,7 +1,11 @@
 package project;
 
 public class HelloWorld {
-    public String handler(String s) {
-        return "Hello, " + s;
+    String dbConnectString = System.getenv("DATABASE_CONNECT_STRING");
+    public void handler(String s) {
+        if (dbConnectString == null || dbConnectString.isEmpty())
+            System.err.println("Hello, " + s + "(No connection string set!");
+        else
+            System.out.println("Hello, " + s + "(" + dbConnectString + ")");
     }
 }
